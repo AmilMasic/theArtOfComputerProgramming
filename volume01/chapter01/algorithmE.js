5e4ffbfb3f198eae11f23a10de4d900b02460837
AmilMasic <amil.masic@gmail.com>
2021-03-06 09:12:36 -0700

    created swapNumbers function to swap the numbers if the second one is bigger than the first

--- volume01/chapter01/algorithmE.js
+++ volume01/chapter01/algorithmE.js
@@ -4,7 +4,24 @@
 function divisor(x,y) {
   let m = x;
   let n = y;
-  console.log(x,y)
+  swapNumbers(m,n);
+
 }
 
 divisor(3,6);
+divisor(10, 9);
+
+//function to swap the position if the second number given is bigger than the first
+function swapNumbers(m,n) {
+  if (n > m) {
+    let r = n;
+    n = m;
+    m = r;
+    console.log("this is the updated n value " + n)
+    console.log("this is the updated m value " + m)
+
+  }
+  else {
+    console.log("X was lower than Y");
+  }
+}
