97de45ccb948e9aa12fa579d7da331f05d2705ae
AmilMasic <amil.masic@gmail.com>
2021-03-06 08:55:31 -0700

    npm init

--- volume01/chapter01/algorithmE.js
+++ volume01/chapter01/algorithmE.js
@@ -1,3 +1,10 @@
 // create an algorithm that will find
 // the highest common divisor between two numbers
 // also know as Euclid's Algorithm
+function divisor(x,y) {
+  let m = x;
+  let n = y;
+  console.log(x,y)
+}
+
+divisor(3,6);
